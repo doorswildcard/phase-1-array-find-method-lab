b785161d7f0f8e970b796e04617487aa7344506a
Ilan Rojas <bobatthevelvetroom@gmail.com>
2023-03-21 18:03:12 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+
+    for (const result in record) {
+        if (record.find(record => record.result === "W")) {
+
+            let yearWon = record.find(record => record.result === "W").year
+            return yearWon
+        }
+    }
+
+    return undefined
+}
